6b44e6a111f1ad00b160e1bc3e56eaebdfb75159
Glymaxus <bastiengiat@gmail.com>
2023-03-06 15:19:54 +0100

    Initial Commit

---
+++ AILRoussetBasket/AILRoussetBasketApp.swift
@@ -0,0 +1,17 @@
+//
+//  AILRoussetBasketApp.swift
+//  AILRoussetBasket
+//
+//  Created by bastien giat on 06/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct AILRoussetBasketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
